b9973e0baaeab736c52891f3c840e76743fb80dc
James Threlfall <jwt104@googlemail.com>
2016-06-24 10:57:16 +0100

    added support for long form names

--- JT_SMTGscript.py
+++ JT_SMTGscript.py
@@ -1,5 +1,12 @@
-#JT 2016 - SMTG publication script, using ADS library (found on github)
+
+# coding: utf-8
+
+# In[1]:
+
 from __future__ import print_function, division
+
+
+# In[2]:
 
 import ads
 import sys
@@ -9,8 +16,14 @@ import os.path
 from StringIO import StringIO
 from time import localtime
 
+
+# In[3]:
+
 # ADS needs a token - this one is for jwt104@googlemail.com so be warned!
 ads.config.token = 'zmmgkG86fM0hBeBjoOJicrr5GegJWe69vJ5ULDNd'
+
+
+# In[4]:
 
 #simple function to read in a file
 def load(fname):
@@ -25,27 +38,37 @@ def load(fname):
 
     return data
 
-#simple function to compare authors with a list and return a hyperlink if a match found.
+
+# In[5]:
+
+# function to compare name against staff list and add hyperlink if match found
 def compareauthor(name,sfile):
     '''compare author with SMTG grp members and return link if matched'''    
     assert type(name) == unicode, 'name must be unicode string'
     assert type(sfile) == str, 'staff filename must be a string'
+    
     
     if os.path.exists(sfile): staff=load(sfile)
     
     found=0
     for people in staff:
         nom=','.join(people[2:3])[2:-1]
+        altnom=', '.join(people[0].split(' ')[::-1])
         if  nom == name.encode("utf-8"):
             #print('FOUND'),
             found=1
             jsthing=''.join(people[4:])
-            #print(jsthing[1:]),
             linktoreturn = '<a href={0}>{1}</a>'.format(jsthing[1:],name)
             type(linktoreturn)
-            #print(linktoreturn),
-            #linktoreturn="<a href=\""+''.join(people[7:])+">"
-            #linktoreturn=linktoreturn+name+"</a>"
+            #fine, thats the normal short form name, what about full name?
+
+        if  found==0 and altnom == name.encode("utf-8"):
+            #print('FOUND'),
+            found=1
+            jsthing=''.join(people[4:])
+            linktoreturn = '<a href={0}>{1}</a>'.format(jsthing[1:],altnom)
+            type(linktoreturn)
+            
 
     if found == 0:
         linktoreturn=name
@@ -54,11 +77,15 @@ def compareauthor(name,sfile):
             
     return linktoreturn
 
+
+# In[17]:
+
 #simple function to output basic html table of correctly formatted authors and titles of SMTG pubs
-def write_table(tlist,alist, filename,stfile):
+def write_table(tlist,alist, dlist, filename,stfile):
     '''output simple html table containing list of recent SMTG pubs and authors'''
     assert type(tlist) == list, 'titlelist must be list of titles'
     assert type(alist) == list, 'authorlist must be list of authors'
+    assert type(dlist) == list, 'doilist must be list of authors'
     assert len(tlist) == len(alist), 'author and title list must be same length'
     assert type(filename) == str, 'name must be string'
     assert type(stfile) == str, 'stafflist file must be a string'
@@ -69,11 +96,9 @@ def write_table(tlist,alist, filename,stfile):
     #pretty_author_name = lambda author: author.split(",")[0] + author.split(",")[1].strip()[1] + "."
     my_string = '' 
     target.write('<HTML><head></head><body>')
-    target.write('<table class="staff" style="border-collapse:collapse;">')
+    target.write('<table>')
     target.write("\n")
-    target.write('<tr><th class="top">Author(s)</th><th class="top">Title</th></tr>')
-    target.write("\n")
-    for title, authors in zip(tlist, alist):    
+    for title, authors, doi in zip(tlist, alist, dlist):    
     #for paper in titlelist:
         target.write('<tr><td>')
         target.write("\n")
@@ -81,15 +106,21 @@ def write_table(tlist,alist, filename,stfile):
             temp=compareauthor(name=author, sfile=stfile)
             target.write(temp.encode('ascii', 'xmlcharrefreplace'))
         
-	target.write("\n")
+        target.write("\n")
         target.write('</td><td>')
-        #target.write(my_string.join(title))
-        target.write(' '.join(title).encode('ascii', 'xmlcharrefreplace'))
+        target.write('<a href=http://dx.doi.org/')
+        target.write(str(doi).encode('ascii')[3:-2])#crop out unicode bits (hardcoded)
+        target.write('>')
+        target.write(str(title).encode('ascii', 'xmlcharrefreplace')[3:-2])
+        target.write('</a>')
         target.write('</td></tr>')
         target.write("\n")
     
     target.write('</table></body></HTML>')
     target.close()
+
+
+# In[18]:
 
 #main prog
 if __name__ == "__main__":
@@ -97,37 +128,38 @@ if __name__ == "__main__":
     print("-------")
     #first check the date and rip out the current month/year
     now = localtime()
-    year, month = (now.tm_year, now.tm_mon - 1)
+    print, now
+    year, month = (now.tm_year, now.tm_mon)
     if 1 > month: year, month = (year - 1, month)
     print('Querying Year/Month {0} (in month {1})'.format(year, month)),
     print(),
 
     #query the location of the current staff list
-    staffloc='../staff_current.txt'
+    staffloc='./staff_current.txt'
+    
     if os.path.exists(staffloc):
-    	staff=load(staffloc) 
+        staff=load(staffloc) 
     else:
         print('NO FILE FOUND AT', staffloc)	
-	exit()
-	
+        exit() 
+    
     outfile='./test.html'
-
     authlist=[]
     titlelist=[]
-
+    doilist=[]
+    
     for person in staff:
         #glue together the peoples name in stafflist
         eachname=','.join(person[2:3])[2:-1]
         # Query ADS for author, institute, pub date, database...
         query = ads.SearchQuery( q='author:\"{0}\" pubdate:{1}         aff:(\"University of St Andrews\") property:refereed          database:("astronomy" OR "physics")'.format(eachname, year),
         #fl=['id', 'first_author', 'year', 'bibcode', 'identifier', 'author','title'],
-        fl=['author','title'])
-
+        fl=['author','title','doi'], rows=10)
         query.execute()
         #count the number of pubs each person has (so far)
         num = int(query.response.numFound)
         print("{person} had {num} publications in {year}".format(person=eachname, num=num, year=year))
-
+        
         #if anything published, then add that to the list
         if num > 0:
             for n in query:
@@ -135,10 +167,13 @@ if __name__ == "__main__":
                 if n.title not in titlelist:
                     titlelist.append(n.title)
                     authlist.append(n.author)
+                    doilist.append(n.doi)
                 #else:
                     #print('matched publication, ignoring..')
     #list is done, so now write out the table, JOB DONE.
-    write_table(tlist=titlelist,alist=authlist,filename=outfile, stfile=staffloc)    
+    
+    write_table(tlist=titlelist,alist=authlist,dlist=doilist,filename=outfile, stfile=staffloc)    
     print("-------")
-    print('DONE')
+    print('DONE: output to {0}'.format(outfile))
+    print('-------')
     print('')
